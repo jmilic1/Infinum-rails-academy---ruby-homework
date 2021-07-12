0a7a88eca843388f702a3446b64efaa8862c5239
jmilic1 <jm51506@fer.hr>
2021-07-12 23:38:55 +0200

    Fix holes

--- lib/holes.rb
+++ lib/holes.rb
@@ -15,31 +15,19 @@
 #   8   |   2
 #   9   |   1
 
-def digit_holes # rubocop:disable Metrics/MethodLength
-  {
-    0 => 1,
-    1 => 0,
-    2 => 0,
-    3 => 0,
-    4 => 1,
-    5 => 0,
-    6 => 1,
-    7 => 0,
-    8 => 2,
-    9 => 1
-  }
-end
+DIGIT_HOLES = {
+  0 => 1,
+  1 => 0,
+  2 => 0,
+  3 => 0,
+  4 => 1,
+  5 => 0,
+  6 => 1,
+  7 => 0,
+  8 => 2,
+  9 => 1
+}
 
 def holes(number)
-  i = 1
-  hole_count = 0
-  holes = digit_holes
-
-  loop do
-    i *= 10
-    hole_count += holes[number % i / (i / 10)]
-    break unless i <= number
-  end
-
-  hole_count
+  number.digits.sum { |digit| DIGIT_HOLES[digit] }
 end
